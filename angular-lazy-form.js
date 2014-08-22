000bd0907fade17229c847a70a3d949b1ad87c41
FedericoElles <github@furikuri.de>
2014-08-22 20:50:27 +0200

    first commit

---
+++ angular-lazy-form.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function lazyFormService(){
+  var lazyFormService = {};
+
+  return lazyFormService;
+}
+
+
+angular.module('angularLazyForm')
+  .factory('lazyFormService', lazyFormService);
